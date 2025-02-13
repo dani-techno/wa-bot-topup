af63421b1155b83a12e0a973750e99390a7e5c1f
Dani Tech. <dani.technology.id@gmail.com>
2025-02-13 09:40:05 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 /**
-* Made by: Dani Technology (Full Stack Engineer)
-* Created on: January 10, 2024
-* Contact developer:
-* - WhatsApp: +62 838-3499-4479 or +62 823-2066-7363
-* - Email: dani.technology.id@gmail.com
-* - GitHub: https://github.com/dani-techno
+- PERINGATAN: PROYEK INI DILINDUNGI OLEH HAK CIPTA DAN LISENSI ISC
+- 
+- Made by: Dani Technology (Full Stack Engineer)
+- Created on: January 10, 2025
+- 
+- KONTAK DEVELOPER:
+-     - WhatsApp: +62 838-3499-4479 or +62 823-2066-7363
+-     - Email: dani.technology.id@gmail.com
+-     - GitHub: https://github.com/dani-techno
+- 
+- PERINGATAN:
+-     - Anda tidak boleh mengklaim proyek ini sebagai milik Anda sendiri.
+-     - Anda tidak boleh menjual proyek ini tanpa izin tertulis dari pemilik hak cipta.
+-     - Anda tidak boleh mengubah atau menghapus atribusi hak cipta dari proyek ini.
+- 
+- KONSEKUENSI PELANGGARAN:
+-     - Ganti rugi atas pelanggaran hak cipta sebesar Rp 1.000.000.000 (satu miliar rupiah) atau lebih.
+-     - Penghentian penggunaan proyek ini dan semua derivatifnya.
+-     - Tindakan hukum lainnya yang sesuai, termasuk tuntutan pidana dan perdata.
+- 
+- DENGAN MENGGUNAKAN PROYEK INI, ANDA MENYATAKAN BAHWA ANDA TELAH MEMBACA, MEMAHAMI, DAN MENYETUJUI SYARAT-SYARAT LISENSI DAN HAK CIPTA INI.
 */
 
 const {
